72a0cf50597d4d8cc87f1ec2fcd3413cc1591bcc
4501104220 <72953715+4501104220@users.noreply.github.com>
2020-10-17 21:28:17 +0700

    Create SACH.h

---
+++ GROUP1_PRO_CTDL_3/GROUP1_PRO_CTDL_3/SACH.h
@@ -0,0 +1,113 @@
+#ifndef SACH_H_INCLUDED_
+#define SACH_H_INCLUDED_
+
+#include "NGAYTHANGNAM.h"
+#include <string>
+using namespace std;
+#pragma once
+
+class Sach
+{
+private:
+	string _strMaSach, _strTieuDe, _strTacGia, _strNhaXuatBan;
+	float _fGiaBan;
+	int _iNamPhatHanh, _iSoTrang;
+	Date NgayNhapKho;
+	int _iTinhTrang;
+public:
+	Sach()
+	{
+		this->_strMaSach = "";
+		this->_strTieuDe = "";
+		this->_strTacGia = "";
+		this->_strNhaXuatBan = "";
+		this->_fGiaBan = 0;
+		this->_iNamPhatHanh = 0;
+		this->_iSoTrang = 0;
+		this->_iTinhTrang = 0;
+	}
+	Sach(string strMaSach):NgayNhapKho()
+	{
+		this->_strMaSach = strMaSach;
+		this->_strTieuDe = "";
+		this->_strTacGia = "";
+		this->_strNhaXuatBan = "";
+		this->_fGiaBan = 0;
+		this->_iNamPhatHanh = 0;
+		this->_iSoTrang = 0;
+		this->_iTinhTrang = 0;
+	}
+	Sach(string strMaSach, string strTieuDe, string strTacGia, string strNhaXuatBan, float fGiaBan, 
+		int iNamPhatHanh, int iSoTrang, int iNgay, int iThang, int iNam, int iTinhTrang)
+		:NgayNhapKho(iNgay, iThang, iNam)
+	{
+		this->_strMaSach = strMaSach;
+		this->_strTieuDe = strTieuDe;
+		this->_strTacGia = strTacGia;
+		this->_strNhaXuatBan = strNhaXuatBan;
+		this->_fGiaBan = fGiaBan;
+		this->_iNamPhatHanh = iNamPhatHanh;
+		this->_iSoTrang = iSoTrang;
+		this->_iTinhTrang = 0;
+	}
+	Sach(string strMaSach, string strTieuDe, string strTacGia, string strNhaXuatBan, float fGiaBan, 
+		int iNamPhatHanh, int iSoTrang, int iTinhTrang)
+	{
+		this->_strMaSach = strMaSach;
+		this->_strTieuDe = strTieuDe;
+		this->_strTacGia = strTacGia;
+		this->_strNhaXuatBan = strNhaXuatBan;
+		this->_fGiaBan = fGiaBan;
+		this->_iNamPhatHanh = iNamPhatHanh;
+		this->_iSoTrang = iSoTrang;
+		this->_iTinhTrang = 0;
+	}
+	string getMaSach()
+	{
+		return this->_strMaSach;
+	}
+	string getTieuDe()
+	{
+		return this->_strTieuDe;
+	}
+	string getTacGia()
+	{
+		return this->_strTacGia;
+	}
+	string getNXB()
+	{
+		return this->_strNhaXuatBan;
+	}
+	float getGiaBan()
+	{
+		return this->_fGiaBan;
+	}
+	int getNamPhatHanh()
+	{
+		return this->_iNamPhatHanh;
+	}
+	int getSoTrang()
+	{
+		return this->_iSoTrang;
+	}
+	Date getNgayNhapKho()
+	{
+		return this->NgayNhapKho;
+	}
+	int getTinhTrang()
+	{
+		return this->_iTinhTrang;
+	}
+	void setTinhTrangSach(int iTinhTrang)
+	{
+		this->_iTinhTrang = iTinhTrang;
+	}
+	friend ostream& operator<<(ostream& os, Sach& sach);
+	void outputSach();
+	void writeSach(ofstream& file);
+	void readSach(ifstream& file);
+	void nhapSachMoi();
+	~Sach() {};
+};
+
+#endif
